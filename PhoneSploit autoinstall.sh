f14e0df3d882d798f7a4d367b770300445fc293c
MrDark111 <73544166+MrDark111@users.noreply.github.com>
2021-06-18 22:33:03 +0530

    Add files via upload

---
+++ PhoneSploit autoinstall.sh
@@ -0,0 +1,77 @@
+echo
+
+
+cd
+
+clear
+
+termux-setup-storage
+
+apt update && upgrade -y
+
+pkg install python -y
+
+pkg install git -y
+
+rm -rf PhoneSploit 
+
+rm -rf phoneSploit
+
+rm -rf Phonesploit 
+
+rm -rf phonesploit 
+
+clear
+
+cd
+
+git clone https://github.com/Uditprabhakar6/PhoneSploit
+
+
+cd PhoneSploit
+
+
+pip install colorama
+
+
+pip2 install colorama
+
+
+
+apt update > /dev/null 2>&1 && apt --assume-yes install wget > /dev/null 2>&1 && wget https://github.com/MasterDevX/Termux-ADB/raw/master/InstallTools.sh -q && bash InstallTools.sh
+
+
+
+cd
+
+
+apt update > /dev/null 2>&1 && apt --assume-yes install wget > /dev/null 2>&1 && wget https://github.com/MasterDevX/Termux-ADB/raw/master/InstallTools.sh -q && bash InstallTools.sh
+
+
+
+cd PhoneSploit 
+
+
+
+ls 
+
+
+clear
+
+
+
+python2 main_linux.py 
+
+
+
+clear
+
+
+
+
+
+echo 
+echo CODE BY MR.DARK
+echo Gmail ID  :-  mr.dark.rv@gmail.com
+echo This is here to only provide educational knowledge Only we do not support any kind of illegal activities like HACKING . User will have to take thier own responsibility before performing any activity On this tool :) :) :) :) :) :) :) :) :) :) :) :) :) :)
+
